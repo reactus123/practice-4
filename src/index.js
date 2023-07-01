a1ba1273675428f50eb651eb5f1f5fb4f3cba6d8
barkode <konstantinks@gmail.com>
2023-07-01 10:12:55 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'modern-normalize';
 
 import { App } from 'components';
 import { GlobalStyles, theme } from 'styles';
-<<<<<<< HEAD
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
 import {persistor, store} from './redux/store'
@@ -22,16 +21,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ThemeProvider>
         {/* </PersistGate> */}
       </Provider>
-=======
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyles} />
-        <App />
-      </ThemeProvider>
-    </Provider>
->>>>>>> 53d29fd6a7d39531e0aa1f5c606496ebb5b0874f
   </React.StrictMode>
 );
