3f3d256e9a15b76e32f121b6fa36d9395b992465
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-07-01 08:45:07 +0100

    create

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import 'modern-normalize';
 
 import { App } from 'components';
 import { GlobalStyles, theme } from 'styles';
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react';
+import {persistor, store} from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+                 <Global styles={GlobalStyles} />
+                  <App />
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
